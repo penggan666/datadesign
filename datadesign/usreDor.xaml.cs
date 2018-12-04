e3c24785636ef97bd063a2ec3ce633a2bc3aea5b
penggan666 <44197448+penggan666@users.noreply.github.com>
2018-12-04 18:05:08 +0800

    宿管人员寝室管理搜索完善

--- datadesign/usreDor.xaml.cs
+++ datadesign/usreDor.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace datadesign
 {
@@ -48,10 +49,36 @@ namespace datadesign
             dta.Columns.Add("女寝or男寝");
             dta.Columns.Add("寝室大小");
             dta.Columns.Add("实际人数");
+            comboBox.ItemsSource = mysql.ExecuteQuery("select roomnum from broom where buildingnum='" + Source.build + "'").DefaultView;
+            comboBox.DisplayMemberPath = "roomnum";
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)//寝室号查询
+        private async void button1_Click(object sender, RoutedEventArgs e)//寝室号查询
         {
+            if (comboBox.Text == "")
+                await this.ShowMessageAsync("提示", "请选择寝室号");
+            else
+            {
+                try
+                {
+                    DataRow drcalc;
+                    dta.Clear();
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        if (dr[1].ToString()==comboBox.Text)
+                        {
+                            drcalc = dta.NewRow();
+                            drcalc.ItemArray = dr.ItemArray;
+                            dta.Rows.Add(drcalc);
+                        }
+                    }
+                    dataGrid.ItemsSource = dta.DefaultView;
+                }
+                catch(Exception ex)
+                {
+                    await this.ShowMessageAsync("错误", ex.ToString());
+                }
+            }
 
         }
     }
